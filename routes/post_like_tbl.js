8fc23ece1098e25bcde6b5ca974714807a2a4360
AbdulMoiz36 <abdulmoiz8236@gmail.com>
2024-08-30 12:16:28 -0700

    pozt likes

--- routes/post_like_tbl.js
+++ routes/post_like_tbl.js
@@ -36,7 +36,7 @@ router.get('/:id', async (req, res) => {
 });
 router.get('/post/:id', async (req, res) => {
     try {
-        const entry = await postLikeTblModel.findOne({ post_id: req.params.id });
+        const entry = await postLikeTblModel.find({ post_id: req.params.id });
         if (!entry) {
             return res.status(404).json({ message: 'Entry not found' });
         }
@@ -45,7 +45,47 @@ router.get('/post/:id', async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
+router.get('/user_post/:userid/:postid', async (req, res) => {
+    try {
+        const { userid, postid } = req.params;
+        
+        // Query to find a document based on both user_id and post_id
+        const entry = await postLikeTblModel.find({ user_id: userid, post_id: postid });
 
+        if (!entry) {
+            return res.status(404).json({ message: 'Entry not found' });
+        }
+
+        res.json(entry);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
+router.post('/', async (req, res) => {
+    const { post_id, user_id } = req.body;
+    
+    if (!post_id || !user_id) {
+      return res.status(400).json({ message: 'Post ID, User ID are required' });
+    }
+  
+    const newLike = new postLikeTblModel({
+      _id: new mongoose.Types.ObjectId(),
+      post_id,
+      user_id,
+      created_at: new Date().toISOString(),
+      updated_at: new Date().toISOString(),
+    });
+  
+    try {
+      const savedLike = await newLike.save();
+      res.status(201).json(savedLike);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+  
 router.delete('/:id', async (req, res) => {
     try {
         const deletedEntry = await postLikeTblModel.findOneAndDelete({ _id: req.params.id });
@@ -57,6 +97,24 @@ router.delete('/:id', async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
+
+// DELETE route to remove a like entry based on user_id and post_id
+router.delete('/:user_id/:post_id', async (req, res) => {
+    try {
+      const { user_id, post_id } = req.params;
+      
+      // Find and delete the like entry
+      const result = await postLikeTblModel.findOneAndDelete({ user_id, post_id });
+  
+      if (!result) {
+        return res.status(404).json({ message: 'Like entry not found' });
+      }
+  
+      res.status(200).json({ message: 'Like removed successfully' });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
 
 router.patch('/:id', async (req, res) => {
     try {
@@ -73,5 +131,8 @@ router.patch('/:id', async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
+
+
+
 
 module.exports = router;
